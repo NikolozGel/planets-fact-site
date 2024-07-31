b254c453d21f62575d010e6461239ad8e8679b00
unknown <nikolozgelenidze9@gmail.com>
2024-07-31 11:29:13 +0400

    delete structure and start over again

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Header from "./assets/components/Header";
 function App() {
   return (
     <>
-      <div className="bg-bg-img w-screen h-screen bg-cover bg-center overflow-hidden bg-[#070724]">
-        <Header />
-      </div>
+      <Header />
     </>
   );
 }
